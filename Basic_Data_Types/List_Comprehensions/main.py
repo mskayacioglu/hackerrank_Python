48ec90bc438182766c4dbcc638379c5b942cfdd6
Mert Samet Kayacioglu <mskayacioglu@icloud.com>
2025-06-20 01:56:15 +0300

    Commit

--- Basic_Data_Types/List_Comprehensions/main.py
+++ Basic_Data_Types/List_Comprehensions/main.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     result = [[i, j, k] 
         for i in range(x + 1) 
     	    for j in range(y + 1) 
-        	for k in range(z + 1) 
-        		if i + j + k != n]
+                for k in range(z + 1)
+                    if i + j + k != n]
 
 print(result)
     
